8fb2d298864fe2941a7ad7be8ebaa39a9c5d5bbf
BrownieMaar <barnamarci07@gmail.com>
2023-01-06 16:09:14 +0100

    create basic page with design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
 
 function App() {
+  const date = new Date();
 
   return (
     <div className="App">
-      <p>This will be the countdown app, which can count past 0. Finally.</p>
+      <div className="clock">
+        {date.getHours()}:{date.getMinutes().toString().padStart(2, "0")}:{date.getSeconds().toString().padStart(2, "0")}
+      </div>
+      <div className='buttons'>
+        <button>Reset</button>
+        <button>Pause</button>
+        <button>Options</button>
+      </div>
+      <div className="options">
+        <div className="option">Some option with text</div>
+        <div className="option inactive">Some option</div>
+        <div className="option">Some option & text</div>
+        <div className="option">Some opt.</div>
+      </div>
     </div>
   )
 }
