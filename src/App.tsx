5a28bdff633a8281b165c971bf8def2615074fd3
BrownieMaar <barnamarci07@gmail.com>
2023-01-07 08:46:27 +0100

    place divs better in landscape

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,18 @@ function App() {
       <div className="clock">
         {date.getHours()}:{date.getMinutes().toString().padStart(2, "0")}:{date.getSeconds().toString().padStart(2, "0")}
       </div>
-      <div className='buttons'>
-        <button>Reset</button>
-        <button>Pause</button>
-        <button>Options</button>
-      </div>
-      <div className="options">
-        <div className="option">Some option with text</div>
-        <div className="option inactive">Some option</div>
-        <div className="option">Some option & text</div>
-        <div className="option">Some opt.</div>
+      <div className='not-clock'>
+        <div className='buttons'>
+          <button>Reset</button>
+          <button>Pause</button>
+          <button>Options</button>
+        </div>
+        <div className="options">
+          <div className="option">Some option with text</div>
+          <div className="option inactive">Some option</div>
+          <div className="option ">Some option & text</div>
+          <div className="option">Some opt.</div>
+        </div>
       </div>
     </div>
   )
